0da0588d85444e8f958e0a74514a7e0b0a9c2195
2NoNe <marinescu_razvan2018@yahoo.com>
2022-11-09 11:39:40 +0200

    nimic

--- PC-anul1/lab-5/p8.c
+++ PC-anul1/lab-5/p8.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main(){
+void arie_intersectie(int x11,int y11,int x12,int y12,int x21,int y21,int x22,int y22){
+
+	int a, b, c, d;
+	int x, y, z, t;
+
+	a = x12;b = y11;c = x11;d = y12;
+	x = x22;y = y21;z = x21;t = y22;
+
+	// printf("%d %d %d %d\n", a, b, c, d);
+	// printf("%d %d %d %d\n", x, y, z, t);
 	
+}
+int main(){
+
+	int a, b, c, d;
+	int x, y, z, t;
+
+	scanf("%d %d %d %d", &a,&b, &c, &d);
+	scanf("%d %d %d %d", &x, &y, &z, &t);
+
+	//printf("%d\n", arie_intersectie(a,b,c,d));
+	arie_intersectie(a,b,c,d,x,y,z,t);
+	return 0;	
 }
