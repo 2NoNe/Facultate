03a5cbd16097f5a506868570deba60e8694a0b70
2NoNe <marinescu_razvan2018@yahoo.com>
2022-11-15 18:43:26 +0200

    lab6

--- PC-anul1/lab-6/p6.c
+++ PC-anul1/lab-6/p6.c
@@ -1,55 +1,76 @@
 #include <stdio.h>
 
+void afisare(int v[], int dim){
+
+    int i;
+
+    printf("\n");
+    
+    for(i=0; i<dim; i++)
+        printf("%d ", v[i]);
+
+    printf("\n");
+}
+
 void spirala(int a[][100], int m, int n)
 {
-    int i, j, k = 0, l = 0;
+    int i, j, v[10000];
+    int rs = 0, re = n-1, cs = 0, ce = m-1;
+ // for(i = 0; i < n; i++){
+ //        for(j = 0; j < m; j++){
+
+ //            printf("%d ", a[i][j]);
+ //        }
+ //        printf("\n");
+ //    }
+
+    j = 0;
+    while (rs <= re && cs <= ce) {
+       // printf("da\n");
+
+        for (i = rs; i <= re; i++) {
+            v[j] = a[i][cs];
+            j++;
+        }
+        cs++;
+ 
+        for (i = cs; i <= ce; i++) {
+            v[j]= a[re][i];
+            j++;
+        }
+        re--;
+ 
+        if (rs < re) {
+            for (i = re; i >= rs; i--) {
+                v[j] = a[i][ce];
+                j++;
+            }
+            ce--;
+        }
+ 
+        if (cs < ce) {
+            for (i = ce; i >= cs; i--) {
+                v[j] = a[rs][i];
+                j++;
+            }
+            rs++;
+        }
+    }
+    afisare(v,j);
+}
+
+int main(){
+
+	int a[100][100], m, n, i, j;
 
     scanf("%d %d", &n, &m);
 
     for(i = 0; i < n; i++){
-    	for(j = 0; j < m; j++){
+        for(j = 0; j < m; j++){
 
-    		scanf("%d", &a[i][j]);
-    	}
-    }
- 
- 
-    while (k < m && l < n) {
-
-        for (i = l; i < n; i++) {
-            printf("%d ", a[k][i]);
-        }
-        k++;
- 
-
-        for (i = k; i < m; i++) {
-            printf("%d ", a[i][n - 1]);
-        }
-        n--;
- 
-
-        if (k < m) {
-            for (i = n - 1; i >= l; i--) {
-                printf("%d ", a[m - 1][i]);
-            }
-            m--;
-        }
- 
-
-        if (l < n) {
-            for (i = m - 1; i >= k; i--) {
-                printf("%d ", a[i][l]);
-            }
-            l++;
+            scanf("%d", &a[i][j]);
         }
     }
-}
-//void afisare (int v[], int dim){
-
-
-int main(){
-
-	int a[100][100], m, n;
 
 	spirala (a, m, n);
 
