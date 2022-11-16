3955e69e25735689337deb9199f7aaaf044de207
2NoNe <marinescu_razvan2018@yahoo.com>
2022-11-16 11:12:18 +0200

    lab6

--- PC-anul1/lab-6/p3.c
+++ PC-anul1/lab-6/p3.c
@@ -4,18 +4,9 @@ void extragere(int a[100],int b[100],int *c,int n,int m){
 
 	int i, j, k = 0;
 
-	scanf("%d", &n);
-	for(i=0; i<n; i++){
-		scanf("%d", &a[i]);
-	}
-	scanf("%d", &m);
-	for(j=0; j<m; j++){
-		scanf("%d", &b[j]);
-	}
-
 	for(i=0; i<n; i++){
 		for(j=0; j<m; j++){
-			
+				
 			if(a[i] == b[j]){
 
 				c[k] = b[j];
@@ -23,9 +14,8 @@ void extragere(int a[100],int b[100],int *c,int n,int m){
 				k++;
 			}
 		}
-
 	}
-
+	
 	printf("\n");
 	printf("%d\n", k);
 
@@ -37,7 +27,16 @@ void extragere(int a[100],int b[100],int *c,int n,int m){
 
 int main(){
 
-	int a[100],b[100],c[100],m,n;
+	int a[100],b[100],c[100],m,n,i,j;
+
+	scanf("%d", &n);
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	scanf("%d", &m);
+	for(j=0; j<m; j++){
+		scanf("%d", &b[j]);
+	}
 
 	extragere(a,b,c,n,m);
 
